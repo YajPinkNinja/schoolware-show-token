9471709636b1d13bb86c8a2a3869023e9d28a16f
YajPinkNinja <36473087+YajPinkNinja@users.noreply.github.com>
2022-05-17 20:10:51 +0200

    Create main.js

---
+++ main.js
@@ -0,0 +1,3 @@
+var div = document.getElementsByClassName("x-container wisa-container-header-logo wisa-logo-schoolware-medium-red x-abs-layout-item x-container-default")[0];
+
+div.innerHTML = `<span style="background-color: lime">Replacement HTML</span>`;
